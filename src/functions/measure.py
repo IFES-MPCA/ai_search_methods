148e65caf8aa61aed523f65f2b427cb9ef9fa307
duraes-antonio <garotoseis@gmail.com>
2022-06-11 14:04:07 -0300

    FEAT: Criar decorator para mensurar tempo de execução da função

---
+++ src/functions/measure.py
@@ -0,0 +1,23 @@
+from functools import wraps
+from time import time
+from typing import Callable
+
+
+def measure(function: Callable) -> Callable:
+    """
+    Define um decorador que mede o tempo de execução de uma função.
+    :param function: função que terá seu tempo de execução medido.
+    :return: função de entrada decorada com a medição de tempo.
+    """
+    @wraps(function)
+    def __time_it__(*args, **kwargs):
+        start = int(round(time() * 1000))
+
+        try:
+            return function(*args, **kwargs)
+
+        finally:
+            end_ = int(round(time() * 1000)) - start
+            print(f"Tempo de execução: {end_ if end_ > 0 else 0} ms")
+
+    return __time_it__
