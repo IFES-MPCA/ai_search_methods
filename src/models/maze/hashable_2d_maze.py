bfc3a806cfa759b84ee09a6f1c8eeaa6722aa848
duraes-antonio <garotoseis@gmail.com>
2022-06-12 11:24:37 -0300

    REFACTOR: Renomear atributos e substituir método de inserir obstaculo por inline

--- src/models/maze/hashable_2d_maze.py
+++ src/models/maze/hashable_2d_maze.py
@@ -5,12 +5,12 @@ from src.models.cell import CellType, Cell, CellPosition
 
 class Hashable2DMaze:
 
-    def __init__(self, n_lines: int, n_columns: int, seed=None, obstacles_chance: float = 0.25):
+    def __init__(self, n_lines: int, n_columns: int, seed=None, obstacles_percent: float = 0.25):
         self.lines = n_lines
         self.columns = n_columns
         self.maze: Dict[Tuple[int, int], Cell] = {}
         self.__generate__(n_lines, n_columns)
-        self.__insert_obstacles__(n_lines, n_columns, obstacles_chance, seed)
+        self.__insert_obstacles__(n_lines, n_columns, obstacles_percent, seed)
 
     def __contains__(self, item: CellPosition) -> bool:
         return item in self.maze
@@ -20,18 +20,16 @@ class Hashable2DMaze:
             for x in range(n_columns):
                 self.maze[(x, y)] = Cell(y, x)
 
-    def __insert_obstacles__(self, n_lines: int, n_columns: int, chance: float = 0.25, seed=None):
+    def __insert_obstacles__(self, n_rows: int, n_columns: int, chance: float = 0.25, seed=None):
         import random
         random.seed(seed)
-        obstacles_count = chance * n_lines * n_columns
+        obstacles_count = chance * n_rows * n_columns
+        range_obstacles = range(int(obstacles_count))
 
-        for _ in range(int(obstacles_count)):
+        for _ in range_obstacles:
             x = random.randint(0, n_columns - 1)
-            y = random.randint(0, n_lines - 1)
-            self.__insert_obstacle__(x, y)
-
-    def __insert_obstacle__(self, x: int, y: int):
-        self.maze[(x, y)].type = CellType.OBSTACLE
+            y = random.randint(0, n_rows - 1)
+            self.maze[(x, y)].type = CellType.OBSTACLE
 
     def set_cell(self, cell: Cell):
         self.maze[(cell.x, cell.y)] = cell
